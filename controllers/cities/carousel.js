d6a20340084929802ce01124e708a5f2c3671a87
inebenitez <101297551+inebenitez@users.noreply.github.com>
2023-08-23 01:06:32 -0300

    sprint#2

--- controllers/cities/carousel.js
+++ controllers/cities/carousel.js
@@ -2,7 +2,7 @@ import City from "../../models/City.js"
 
 export default async(req,res,next)=> {
     try {
-        let all = await City.find({}, 'city photo').sort({ funsation:-1 }).limit(15)
+        let all = await City.find({}, 'city photo').sort({ funsation:-1 }).limit(16)
         let count = await City.countDocuments()
         return res.status(200).json({
             success: true,
