ce30a639befef40dbfab40b97d8f13357c7e1279
inebenitez <101297551+inebenitez@users.noreply.github.com>
2023-09-11 18:43:30 -0300

    activities ok

--- controllers/activities/read.js
+++ controllers/activities/read.js
@@ -1,19 +1,23 @@
 import Activity from "../../models/Activity.js";
 
-export default async (req, res)=>{
-    try{
-        let allAct = await Activity.find()
+export default async(req, res, next)=>{
+    try {
+        let queries = {}
+        if (req.query.itinerary_id){
+            queries.itinerary_id = req.query.itinerary_id
+        }
+        let all = await Activity.find(queries)
+        .populate({
+            path: "itinerary_id",
+            select: "name photo _id"
+        })
         return res.status(200).json({
             success: true,
-            message:'activity found',
-            response: allAct
-        })
+            message: "activities found",
+            response: all
 
-    }catch(error){
-        return res.status(400).json({
-            success: false,
-            message:'activity NOT found',
-            response: null
         })
+    } catch (error) {
+        next(error)
     }
 }
